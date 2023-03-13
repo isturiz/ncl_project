c775eb75652e660210b30278b63631739f352a4b
isturiz <isturizmauricio@gmail.com>
2023-03-13 13:02:03 -0400

    add models

--- ncl_app/ncl_app/settings.py
+++ ncl_app/ncl_app/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # LOCAL APPS
+    'ncl.apps.NclConfig',
+
+    # DJANGO APPS
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
