2ff5dd7e90bcc6a3a77c6085d2930bfacaadc4b7
isturiz <isturizmauricio@gmail.com>
2023-03-14 00:40:40 -0400

    in progress: first view load: header

--- ncl_app/ncl/views.py
+++ ncl_app/ncl/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Representative
+
 def index(request):
-    return HttpResponse("hola mundo")
+    return HttpResponse("Main page")
+
+
+def home(request):
+    return render(request, "ncl/home.html", {})
+
+
+def detail(request, representative_id):
+    representative_list = Representative.objects.all()
+    return render(request, "ncl/detail.html", {
+        "representative_list": representative_list
+    })
