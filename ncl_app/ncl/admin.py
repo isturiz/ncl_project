a102823daa470d0441399fa0508004568b8e4b10
isturiz <isturizmauricio@gmail.com>
2023-03-15 00:44:23 -0400

    add total income in payment and home

--- ncl_app/ncl/admin.py
+++ ncl_app/ncl/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Representative, Student
 from .models import Teacher
+from .models import Payment
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'phone_number', 'email', 'representative')
@@ -10,6 +11,10 @@ class TeacherAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'phone_number', 'email')
 
 
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'payment_date', 'reference_number', 'amount', 'description')
+
 admin.site.register(Representative)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Payment, PaymentAdmin)
--- ncl_app/ncl/views.py
+++ ncl_app/ncl/views.py
@@ -1,39 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Sum
 
 from .models import Representative, Student
 from .models import Teacher
+from .models import Payment
 
 def index(request):
     return HttpResponse("Main page")
 
 def home(request):
     total_student = Student.objects.count()
-    return render(request, 'ncl/home/home.html', 
-        {'total_student': total_student}
-    )
+    total_income = Payment.objects.all().aggregate(Sum('amount'))['amount__sum']
+    return render(request, 'ncl/home/home.html', {
+        'total_student': total_student,
+        'total_income': total_income
+    })
 
 def student(request):
     student_list = Student.objects.all()
     return render(request, 'ncl/student/student.html', {
-        "student_list": student_list
+        'student_list': student_list
     })
 
 
 def representative(request):
     representative_list = Representative.objects.all()
     return render(request, 'ncl/representative/representative.html', {
-        "representative_list": representative_list
+        'representative_list': representative_list
     })
 
 def teacher(request):
     teacher_list = Teacher.objects.all()
     return render(request, 'ncl/teacher/teacher.html', {
-        "teacher_list": teacher_list
+        'teacher_list': teacher_list
     })
 
 def payment(request):
-    payment_list = payment.objects.all()
+    payment_list = Payment.objects.all()
+    total_income = payment_list.aggregate(Sum('amount'))['amount__sum']
     return render(request, 'ncl/payment/payment.html', {
-        "payment_list": payment_list
+        'payment_list': payment_list,
+        'total_income': total_income
     })
